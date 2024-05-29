9c87262e24ac6ba0cbac9aa402fd6cc615b1e0fa
Keyur Patel <Kpatel0092@conestogac.on.ca>
2024-05-30 02:03:01 +0530

    feat: User models added

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,5 +1,5 @@
 export const config = {
-    mongodb_uri: 'mongodb://localhost:27017/prod_db',
+    mongodb_uri: "mongodb+srv://medimanage:medimanagepassword@medimanage.i4dogq5.mongodb.net/medimanage?retryWrites=true&w=majority&appName=MediManage",
     port: process.env.PORT || 3000,
     logLevel: 'info'
 };
--- src/types/MongooseOptions.ts
+++ src/types/MongooseOptions.ts
@@ -3,6 +3,4 @@ import { ConnectOptions } from "mongoose";
 export interface MongooseOptions extends ConnectOptions {
     useNewUrlParser?: boolean;
     useUnifiedTopology?: boolean;
-    useCreateIndex?: boolean;
-    useFindAndModify?: boolean;
 }
