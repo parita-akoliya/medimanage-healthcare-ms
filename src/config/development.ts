17f4ea60c9fcfd36448f2f4a1a8c7f93b19274ca
Utsav Padariya <upadariya1180@conestogac.on.ca>
2024-06-04 03:05:02 +0530

    auth endpoints added

--- src/config/development.ts
+++ src/config/development.ts
@@ -1,5 +1,16 @@
 export const config = {
     mongodb_uri: "mongodb+srv://medimanage:medimanagepassword@medimanage.i4dogq5.mongodb.net/medimanage?retryWrites=true&w=majority&appName=MediManage",
     port: 3000,
-    logLevel: 'debug'
+    logLevel: 'debug',
+    jwtSecret: 'medimanage',
+    mailer: {
+        email: "medimanagehealthcare@gmail.com",
+        fromAlias: "MediManageHealthCare",
+        password: "urud hsix yalp rmwj",
+        host:"smtp.gmail.com",
+        port: 465,
+        service: "Gmail",
+        secure: true,
+
+    }
 };
--- src/routes/setupRoutes.ts
+++ src/routes/setupRoutes.ts
@@ -1,19 +1,19 @@
 import { Application } from 'express';
-import authRoutes from './route-configs/auth.route-config';
 import { validate } from '../middleware/validationMiddleware';
 import { logger } from '../utils/logger';
+import authRoutes from './route-configs/auth.route-config';
 
 const loadRoutes = [authRoutes]
 
 export function setupRoutes(app: Application) {
     loadRoutes.forEach(routes => {
-      routes.forEach((route) => {
-        logger.info(`Loading ${route.method} for ${route.path} with validation ${route.requestBodyValidation}`)
-        if (route.joiValidationSchema) {
-            app[route.method](route.path, validate(route.joiValidationSchema), route.controller);
-        } else {
-            app[route.method](route.path, route.controller);
-        }
-    });  
+        routes.forEach((route) => {
+            logger.info(`Loading ${route.method} for ${route.path} with validation ${route.requestBodyValidation}`)
+            if (route.joiValidationSchema) {
+                app[route.method](`${route.path}`, validate(route.joiValidationSchema), route.controller);
+            } else {
+                app[route.method](`${route.path}`, route.controller);
+            }
+        });
     });
 }
